df33decc47a20f07a25bdb32993086c7c00ca264
Simen H <simenh.jobb@gmail.com>
2014-05-18 17:35:15 +0200

    Simplifications

--- problem62.py
+++ problem62.py
@@ -6,63 +6,6 @@
 # Solution #
 ############
 
-import math
-
-def getDigitCounts(n):
-	count = [0] * 10
-	while n > 0:
-		count[n % 10] += 1
-		n = n // 10
-	return count
-
-def isPermutation(a, b):
-	for i in range(10):
-		if a[i] != b[i]:
-			return False
-	return True
-	# count_a = [0] * 10
-	# count_b = [0] * 10
-	# while a > 0 and b > 0:
-	# 	d_a = a % 10
-	# 	d_b = b % 10
-	# 	count_a[d_a] += 1
-	# 	count_b[d_b] += 1
-	# 	a = a // 10
-	# 	b = b // 10
-	# if a != b:
-	# 	return False
-	# for i in range(10):
-	# 	if count_a[i] != count_b[i]:
-	# 		return False
-	# return True
-
-def nextPermutation(p):
-	n = len(p)
-	i = n - 1
-	while i > 0 and p[i - 1] >= p[i]:
-		i -= 1
-
-	if i == 0:
-		return [] # done
-
-	pivot = i - 1
-
-	# Find smallest number that is greater than pivot
-	j = n - 1
-	while j > 0 and p[j] <= p[pivot]:
-		j -= 1
-
-	p[pivot], p[j] = p[j], p[pivot]
-	p[i:] = reversed(p[i:])
-	return p
-
-def pow10(n):
-	result = 1
-	while n > 0:
-		result *= 10
-		n -= 1
-	return result
-
 def getDigits(n):
 	digits = []
 	while n > 0:
@@ -76,10 +19,6 @@ def getNumber(digits):
 		number *= 10
 		number += d
 	return number
-
-def isCubed(n):
-	i = int(math.pow(n, 1 / 3))
-	return (i + 1) * (i + 1) * (i + 1) == n or i * i * i == n
 
 upper_limit = 100000 # Guessing
 permutations = {}
@@ -103,80 +42,3 @@ for key, value in permutations.items():
 			smallest = smallest_cubes[key]
 print(smallest)
 
-# def solve(n, nnn, limit, ncounts, level):
-# 	m = n
-# 	mmm = m * m * m
-# 	while mmm <= limit:
-# 		m += 1
-# 		mmm = m * m * m
-
-# 		mcounts = getDigitCounts(mmm)
-# 		if not isPermutation(ncounts, mcounts):
-# 			continue
-# 		# print(m, mmm)
-# 		d = getDigits(mmm)
-# 		count = 0
-# 		while len(d) > 0:
-# 			if isCubed(getNumber(d)):
-# 				count += 1
-# 			d = nextPermutation(d)
-# 		if count == 4:
-# 			return True
-# 		# print(mmm, "done")
-# 		# if level == 4:
-# 		# 	return True
-# 		# else:
-# 		# 	return solve(m, mmm, pow10(int(math.log10(mmm)) + 1) * 10, mcounts, level + 1)
-# 	return False
-
-# for n in range(2, 10000):
-# 	nnn = n * n * n
-# 	limit = pow10(int(math.log10(nnn)) + 1) * 10
-# 	ncounts = getDigitCounts(nnn)
-# 	if solve(n, nnn, limit, ncounts, 2):
-# 		print(n, n * n * n)
-# 		break
-
-## NOPE
-# for n in range(2, 1000):
-# 	nnn = n * n * n
-# 	p = getDigits(nnn)
-# 	count = 0
-# 	print(nnn)
-# 	while len(p) > 0:
-# 		if isCubed(getNumber(p)):
-# 			if getNumber(p) != nnn:
-# 				print("ok ", getNumber(p))
-# 			count += 1
-# 		p = nextPermutation(p)
-# 	if count == 3:
-# 		print(n)
-# 		break
-
-# def solve():
-# 	for n in range(2, 500):
-# 		nnn = n * n * n
-# 		limit = pow10(int(math.log10(nnn)) + 1) * 10
-# 		for m in range(n + 1, limit):
-# 			mmm = m * m * m
-
-# 			if not isPermutation(mmm, nnn):
-# 				continue
-
-# 			limitl = pow10(int(math.log10(mmm)) + 1) * 10
-# 			for l in range(m + 1, limitl):
-# 				lll = l * l * l
-# 				if isPermutation(lll, nnn):
-# 					return n, nnn
-# 			# count = 0
-# 			# p = getDigits(mmm)
-# 			# p = nextPermutation(p)
-# 			# print(p)
-# 			# while len(p) > 0:
-# 			# 	if isCubed(getNumber(p)):
-# 			# 		count += 1
-# 			# 	p = nextPermutation(p)
-# 			# if count == 1:
-# 			# 	return n, nnn
-
-# print(solve())
